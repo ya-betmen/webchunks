6e431fa9fa1bd4de178f76e963eeb75d563cd7fd
petr <petr@misha-MS-7786>
2014-10-18 00:35:44 +0600

    Первая версия сайта

---
+++ js/main.js
@@ -0,0 +1,30 @@
+﻿function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+            ));
+    var result = matches ? decodeURIComponent(matches[1]) : undefined;
+    return result;
+}
+
+function setCookie(name, value, options) {
+    options = options || {'path':'/'};
+    var expires = options.expires;
+    if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+    value = encodeURIComponent(value);
+    var updatedCookie = name + "=" + value;
+    for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+        }
+    }
+    document.cookie = updatedCookie;
+} 
